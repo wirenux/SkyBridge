6278934710a7d14624b2a5c90ce8960ea1d2df65
Louis Lesniak <160175686+wirenux@users.noreply.github.com>
2026-05-02 18:16:20 +0200

    Change server port from 5001 to 5000

--- server/main.py
+++ server/main.py
@@ -7,4 +7,4 @@ t = threading.Thread(target=udp_listener.start, daemon=True)
 t.start()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001, use_reloader=False)
+    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
