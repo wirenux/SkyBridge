5cf3008c80cbc76042831da34806c28c3a9a2409
Louis Lesniak <160175686+wirenux@users.noreply.github.com>
2026-05-02 22:49:44 +0200

    Integrate socketio and modify server startup

--- server/main.py
+++ server/main.py
@@ -1,10 +1,10 @@
 import threading
 
 import udp_listener
-from app import app
-
-t = threading.Thread(target=udp_listener.start, daemon=True)
-t.start()
+from app import app, socketio
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
+    udp_listener.init(socketio)
+    t = threading.Thread(target=udp_listener.start, daemon=True)
+    t.start()
+    socketio.run(app, debug=True, host="0.0.0.0", port=5000, use_reloader=False)
